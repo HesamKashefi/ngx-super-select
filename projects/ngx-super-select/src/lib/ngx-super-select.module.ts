6a5639f07a1366fc074a7de3ac8042bce2abaa58
Hesam Kashefi <hesamkashefi@yahoo.com>
2023-09-18 09:38:10 +0330

    added NO_SCHEMA_ERROR

--- projects/ngx-super-select/src/lib/ngx-super-select.module.ts
+++ projects/ngx-super-select/src/lib/ngx-super-select.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { NgxSuperSelectComponent } from './ngx-super-select.component';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
@@ -15,6 +15,9 @@ import { CommonModule } from '@angular/common';
   ],
   exports: [
     NgxSuperSelectComponent
+  ],
+  schemas: [
+    NO_ERRORS_SCHEMA
   ]
 })
 export class NgxSuperSelectModule { }
